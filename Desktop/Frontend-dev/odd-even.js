f92c7b2948070ef61370265ad5a479961afb9d74
Teeshakul <teesha119@gmail.com>
2025-12-04 11:19:47 +0530

    [Teesha] Added odd-even.js program

---
+++ Desktop/Frontend-dev/odd-even.js
@@ -0,0 +1,16 @@
+let numbers = [];
+let result = [];
+
+for (let i = 1; i <= 30; i++) {
+    numbers.push(i);
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        result.push("FizzBuzz");
+    } else if (i % 2 === 0) {
+        result.push("Even");
+    } else {
+        result.push("Odd");
+    }
+}
+
+console.log(result);
